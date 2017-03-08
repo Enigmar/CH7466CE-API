7b314bf38636da3ffcfc9859e449bf0ef31eace4
Niklas Linz <niklaslinz@hotmail.com>
2017-03-08 14:27:21 +0100

    Now working on raspberry pi 1

--- src/main/java/de/linzn/cbn/ch7466ce/CBNApi.java
+++ src/main/java/de/linzn/cbn/ch7466ce/CBNApi.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HTMLParserListener;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
@@ -17,10 +16,12 @@ public class CBNApi {
 	
 	
 	public void restartCBN(String cbnIP, String cbnUsername, String cbnPassword){
-		Boolean status;
-		status = this.login_cbn_webinterface(cbnIP, cbnUsername, cbnPassword);
-		System.out.println("Login: " + status);
-		
+		WebClient client = this.login_cbn_webinterface(cbnIP, cbnUsername, cbnPassword);
+		boolean status = this.restart_cbn_webinterface(client, cbnIP);
+		client.close();
+		if (status){
+			System.out.println("Restart Succesfuly!");
+		}
 	}
 	
 	
@@ -29,9 +30,26 @@ public class CBNApi {
 	// Private functions
 	
 	
-	private boolean login_cbn_webinterface(String cbnIP, String cbnUsername, String cbnPassword) {
+	public boolean restart_cbn_webinterface(WebClient webClient, String cbnIP){
 		try {
-			WebClient webClient = new WebClient(BrowserVersion.CHROME);
+		webClient.waitForBackgroundJavaScript(4000);
+		HtmlPage restartPage = (HtmlPage)webClient.getPage("http://" + cbnIP + "/common_page/reboot.html");
+	    webClient.waitForBackgroundJavaScript(4000);
+		System.out.println(restartPage.asText());
+	    webClient.waitForBackgroundJavaScript(4000);
+	    webClient.getOptions().setTimeout(4000);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Es wurde kein Content registriert. Fehler 2!");
+			return false;
+		}
+		return true;
+	}
+	
+	private WebClient login_cbn_webinterface(String cbnIP, String cbnUsername, String cbnPassword) {
+		WebClient webClient = null;
+		try {
+			webClient = new WebClient(BrowserVersion.CHROME);
 			webClient.getOptions().setCssEnabled(false);
 			webClient.getOptions().setUseInsecureSSL(true);
 			webClient.getOptions().setThrowExceptionOnScriptError(false);
@@ -41,7 +59,6 @@ public class CBNApi {
 			HtmlPage loginPage = (HtmlPage)webClient.getPage("http://" + cbnIP + "/common_page/login.html");
 		    webClient.waitForBackgroundJavaScript(500);
 			HtmlForm form = loginPage.getHtmlElementById("form-login");
-
 			HtmlTextInput userField = form.getInputByName("loginUsername");
 			userField.setValueAttribute(cbnUsername);
 		    HtmlPasswordInput passField = form.getInputByName("loginPassword");
@@ -50,18 +67,11 @@ public class CBNApi {
 		    webClient.waitForBackgroundJavaScript(2000);
 			System.out.println(loginPage.asText());
 		    webClient.getOptions().setTimeout(1000);
-			loginPage = (HtmlPage)webClient.getPage("http://" + cbnIP + "/common_page/reboot.html");
-		    webClient.waitForBackgroundJavaScript(2000);
-			System.out.println(loginPage.asText());
-		    webClient.waitForBackgroundJavaScript(2000);
-		    webClient.getOptions().setTimeout(1000);
-			webClient.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("Es wurde kein Content registriert. Fehler!");
-			return false;
 		}
-		return true;
+		return webClient;
 	}
 
 }
