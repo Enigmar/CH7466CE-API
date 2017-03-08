56f2ac4a9db75cef232ffdf4b8aed113b662ef81
Niklas Linz <niklaslinz@hotmail.com>
2017-03-08 20:19:34 +0100

    Disable error message

--- src/main/java/de/linzn/cbn/ch7466ce/CBNApi.java
+++ src/main/java/de/linzn/cbn/ch7466ce/CBNApi.java
@@ -1,6 +1,10 @@
 package de.linzn.cbn.ch7466ce;
 
 import java.io.IOException;
+import java.util.logging.Level;
+
+import org.apache.commons.logging.LogFactory;
+
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
@@ -10,66 +14,81 @@ import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class CBNApi {
-	
+
+	private String cbnIP;
+	private String cbnUsername;
+	private String cbnPassword;
+
+	public CBNApi(String cbnIP, String cbnUsername, String cbnPassword) {
+		this.cbnIP = cbnIP;
+		this.cbnUsername = cbnUsername;
+		this.cbnPassword = cbnPassword;
+
+	}
 
 	// Public API
-	
-	
-	public void restartCBN(String cbnIP, String cbnUsername, String cbnPassword){
-		WebClient client = this.login_cbn_webinterface(cbnIP, cbnUsername, cbnPassword);
-		boolean status = this.restart_cbn_webinterface(client, cbnIP);
-		client.close();
-		if (status){
-			System.out.println("Restart Succesfuly!");
-		}
-	}
-	
-	
 
-	
+	public void restartCBN() {
+		WebClient client = this.login_cbn_webinterface(this.cbnIP, this.cbnUsername, this.cbnPassword);
+		boolean status = this.restart_cbn_webinterface(client, this.cbnIP);
+		client.close();
+	}
+
 	// Private functions
-	
-	
-	public boolean restart_cbn_webinterface(WebClient webClient, String cbnIP){
+
+	public boolean restart_cbn_webinterface(WebClient webClient, String cbnIP) {
 		try {
-		webClient.waitForBackgroundJavaScript(4000);
-		HtmlPage restartPage = (HtmlPage)webClient.getPage("http://" + cbnIP + "/common_page/reboot.html");
-	    webClient.waitForBackgroundJavaScript(4000);
-		System.out.println(restartPage.asText());
-	    webClient.waitForBackgroundJavaScript(4000);
-	    webClient.getOptions().setTimeout(4000);
+			System.out.println("Send restart signal to CBN-Modem...Take some while!");
+			webClient.waitForBackgroundJavaScript(4000);
+			HtmlPage restartPage = (HtmlPage) webClient.getPage("http://" + cbnIP + "/common_page/reboot.html");
+			webClient.waitForBackgroundJavaScript(4000);
+			
+			//System.out.println(restartPage.asText());
+			webClient.waitForBackgroundJavaScript(4000);
+			webClient.getOptions().setTimeout(4000);
+			System.out.println("Signal send finish!");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("Es wurde kein Content registriert. Fehler 2!");
+			System.out.println("No CBN Page found!");
 			return false;
 		}
 		return true;
 	}
-	
+
 	private WebClient login_cbn_webinterface(String cbnIP, String cbnUsername, String cbnPassword) {
 		WebClient webClient = null;
 		try {
+			// disable error logging
+			LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog"); 
+			java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF); 
+			java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF); 
+			java.util.logging.Logger.getLogger("org.apache.http.client.protocol.ResponseProcessCookies").setLevel(Level.OFF);
+			
+			System.out.println("Create connection to CBN-Modem...Take some while!");
 			webClient = new WebClient(BrowserVersion.CHROME);
 			webClient.getOptions().setCssEnabled(false);
 			webClient.getOptions().setUseInsecureSSL(true);
 			webClient.getOptions().setThrowExceptionOnScriptError(false);
 			webClient.getCookieManager().setCookiesEnabled(true);
-		    webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-		    webClient.getOptions().setTimeout(1000);
-			HtmlPage loginPage = (HtmlPage)webClient.getPage("http://" + cbnIP + "/common_page/login.html");
-		    webClient.waitForBackgroundJavaScript(500);
+			webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+			webClient.getOptions().setTimeout(1000);
+			System.out.println("Open login to CBN-Modem...Take some while!");
+			HtmlPage loginPage = (HtmlPage) webClient.getPage("http://" + cbnIP + "/common_page/login.html");
+			webClient.waitForBackgroundJavaScript(500);
 			HtmlForm form = loginPage.getHtmlElementById("form-login");
 			HtmlTextInput userField = form.getInputByName("loginUsername");
 			userField.setValueAttribute(cbnUsername);
-		    HtmlPasswordInput passField = form.getInputByName("loginPassword");
+			HtmlPasswordInput passField = form.getInputByName("loginPassword");
 			passField.setValueAttribute(cbnPassword);
 			loginPage = form.getInputByValue("Login").click();
-		    webClient.waitForBackgroundJavaScript(2000);
-			System.out.println(loginPage.asText());
-		    webClient.getOptions().setTimeout(1000);
+			System.out.println("Logging in to CBN-Modem...Take some while!");
+			webClient.waitForBackgroundJavaScript(2000);
+			//System.out.println(loginPage.asText());
+			webClient.getOptions().setTimeout(1000);
+			System.out.println("Login finish!");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("Es wurde kein Content registriert. Fehler!");
+			System.out.println("No CBN Page found!");
 		}
 		return webClient;
 	}
--- src/main/java/de/linzn/cbn/ch7466ce/CHControl.java
+++ src/main/java/de/linzn/cbn/ch7466ce/CHControl.java
@@ -1,56 +1,51 @@
 package de.linzn.cbn.ch7466ce;
 
 public class CHControl {
-	private static  CHControl instance;
-	
-	private String cbnIP = "192.168.0.1";
-	private String cbnUsername = "admin";
-	private String cbnPassword = "password";
-	
+	private static CHControl instance;
+
 	private CBNApi cbnApi;
-	
 
 	public static void main(String[] args) {
 		instance = new CHControl(args);
 
 	}
-	
-	public CHControl(String[] args){
-		cbnApi = new CBNApi();
-	
-		
-		if (args.length >= 4){
-			this.cbnPassword = args[3];
-			this.cbnUsername = args[2];
-			this.cbnIP = args[1];
-		} else if (args.length == 3){
-			this.cbnUsername = args[2];
-			this.cbnIP = args[1];
-		} if (args.length == 2){
-			this.cbnIP = args[1];
-		} if (args.length < 1){
+
+	public CHControl(String[] args) {
+		String cbnIP = "192.168.0.1";
+		String cbnUsername = "admin";
+		String cbnPassword = "password";
+
+		if (args.length >= 4) {
+			cbnPassword = args[3];
+			cbnUsername = args[2];
+			cbnIP = args[1];
+		} else if (args.length == 3) {
+			cbnUsername = args[2];
+			cbnIP = args[1];
+		}
+		if (args.length == 2) {
+			cbnIP = args[1];
+		}
+		if (args.length < 1) {
 			System.out.println("Argumente: (IP-Adresse) (CBNUser) (CBNPassword)");
 			return;
 		}
-		
+
 		String command = args[0];
-		
+
+		this.cbnApi = new CBNApi(cbnIP, cbnUsername, cbnPassword);
+
 		switch (command) {
-		
+
 		case "restart":
-			this.cbnApi.restartCBN(this.cbnIP, this.cbnUsername, this.cbnPassword);
-		
+			this.cbnApi.restartCBN();
+
 		}
-		
-		
+
 	}
 
-	
-	
-	
-
-	public static CHControl getInstance(){
+	public static CHControl getInstance() {
 		return instance;
 	}
-	
+
 }
